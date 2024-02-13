f3c7f017125efe8cb856c6d807b918c48a319722
Sofia <putlyuksophia@gmail.com>
2024-02-13 20:17:58 +0200

    hw-variable

---
+++ script.js
@@ -0,0 +1,38 @@
+/*Task 1*/
+let age = 14;
+console.log(age);
+/*Task 2*/
+let name = "Sophia";
+console.log(name);
+/*Task 3*/
+let isStudent = false;
+isStudent = true;
+console.log(isStudent);
+/*Task 4*/
+let myString;
+myString =
+  "Our great disadvantage is that we give up too quickly. The surest way to success – all the time to try one more time.";
+console.log(myString);
+/*Task 5*/
+let myNumber = 5;
+myNumber = myNumber + 10;
+console.log(myNumber);
+/*Task 6*/
+let myNull = null;
+console.log(myNull);
+/*Task 7*/
+let userName = prompt("Будь ласка, введіть своє ім'я:");
+alert("Привіт, " + userName + "!");
+/*Task 8*/
+if (confirm("Бажаєте підтвердити дію?")) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+/*Task 9*/
+alert("УВАГА! Ця дія є небезпечною.");
+if (confirm("Бажаєте підтвердити дію?")) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
