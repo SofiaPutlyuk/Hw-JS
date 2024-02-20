238c822295d01e8130deb132bef1d99388ccaaa3
Sofia <putlyuksophia@gmail.com>
2024-02-20 17:49:35 +0200

    hw/math-operation

---
+++ index.js
@@ -0,0 +1,43 @@
+/*Task 1 */
+temperature_celsius = 20;
+temperature_fahrenheit = (temperature_celsius * 9) / 5 + 32;
+console.log(temperature_fahrenheit);
+/*Task 2 */
+days_in_month = 30;
+hours_in_day = 24;
+minutes_in_hour = 60;
+total_hours = days_in_month * hours_in_day;
+total_minutes = total_hours * minutes_in_hour;
+console.log(total_minutes, total_hours);
+/*Task 3 */
+const health = 100;
+const energy = 50;
+console.log(health - energy);
+/*Task 4 */
+const purchaseAmount = 1000;
+let discount = purchaseAmount * 0.1;
+let discountedAmount = purchaseAmount - discount;
+console.log(discountedAmount);
+/*Task 5 */
+let floatingNumber = 3.7;
+let roundedNumber = Math.floor(floatingNumber);
+console.log(roundedNumber);
+/*Task 6 */
+let floatString = "3.14";
+let floatNumber = parseFloat(floatString);
+console.log(floatNumber);
+/*Task 7 */
+let intString = "42";
+let intNumber = parseInt(intString);
+console.log(intNumber);
+/*Task 8 */
+let number = 16;
+let squareRoot = Math.sqrt(number);
+console.log(squareRoot);
+/*Task 9 */
+let integerNumber = 42;
+let stringNumber = "123";
+let parsedNumber = parseInt(stringNumber);
+console.log(parsedNumber);
+let numberToString = integerNumber.toString();
+console.log(numberToString);
