8a36ba1ac389c8dd1fb3a79042e5377b24fd479b
Sofia <putlyuksophia@gmail.com>
2024-02-29 15:41:08 +0200

    hw-rows

---
+++ index.js
@@ -0,0 +1,19 @@
+/*Task 1 */
+const result = 5 + 5 + "5";
+console.log (result);
+console.log (typeof result);
+/*Task 2 */
+const email = "@sofiaputlyuk";
+console.log (email.includes('@'));
+console.log(email.length);
+/*Task 3 */
+let text1 = 'My ';
+let text2 = 'name ';
+let text3 = 'is ';
+let fullName = text1  + text2  + text3 +'Viktor';
+console.log (fullName);
+/*Task 4 */
+const userName = "Олександро";
+const payment = 300;
+let checkProduct = alert(`Дякуємо , ${userName}! До сплати ${payment} гривень `);
+console.log(checkProduct);
