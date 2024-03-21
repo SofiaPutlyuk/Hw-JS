8d3d2c1696f468683c2431376bbaac50bdc70020
Sofia <putlyuksophia@gmail.com>
2024-03-21 18:16:30 +0200

    hw-cycles

---
+++ index.js
@@ -0,0 +1,37 @@
+/*Task 1 */
+let num = 10 ;
+while (num) {
+alert(num);
+num-=1;
+}
+/*Task 2 */
+for(let i = 2 ; i < 20 ; i+=1) {
+if (i%2==1) {
+continue;
+}
+}
+/*Task 3*/
+for (let i = 0 ; i < 63; i+=7){
+ if(i*7==1){
+ }
+ console.log(i)
+}
+/*Task 7 */
+let num2 = 20 ;
+while (num2){
+num2-=1;
+if(num2===7){
+break;
+            }
+console.log(num2)
+   
+}
+/*Task 8 */
+let num3 = 20;
+while (num3){
+num3-=1;
+if(num3 % 3==1){
+ continue;
+}
+console.log(num3)
+}
