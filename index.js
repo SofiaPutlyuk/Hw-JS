61fa766d8f92584a6daa7113df4d7721dcaa409c
Sofia <putlyuksophia@gmail.com>
2024-04-23 17:38:43 +0300

    fix/hw-methods

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /*Task1 */
 const friends = [ "Mango", "Poly", "Kiwi", "Ajax"];
 let string = "";
-
 for (let i = 0; i < friends.length; i++) {
 string += friends[i] + ", ";
+if (i!== friends.length) {
+    string += ", ";
+  } 
 }
 
 console.log(string);
@@ -11,13 +13,13 @@ console.log(friends.join(','));
 
 /*Task 2 */
 const cards = ["Карточка - 1 " , "Карточка - 2", "Карточка - 3", "Карточка - 4", "Карточка - 5"]
-const card = cards.indexOf("Карточка - 3")
-console.log(card)
-const cardToRemove = cards.splice(2,1)
+const cardIdx = cards.indexOf("Карточка - 3")
+console.log(cardIdx)
+const cardToRemove = cards.splice(cardIdx,1)
 console.log(cards)
-const cardInsert = cards.splice(3,0,'Карточка-6')
+const cardInsert = cards.splice(cardIdx + 1,0,'Карточка-6')
 console.log(cards)
-const cardToUpdate = cards.splice(4,1,'Карточка -7')
+const cardToUpdate = cards.splice(cardIdx + 2,1,'Карточка -7')
 console.log(cards)
 
 
