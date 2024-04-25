6548e03a65b3653a9529a767ca44e957c3906c26
Sofia <putlyuksophia@gmail.com>
2024-04-25 14:27:14 +0300

    fix/hw-arrays2

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 /*Task1*/
-const num = [1,2,3];
-num[1]=10;
+const num = [1, 2, 3];
+num[1] = 10;
 console.log(num[1]);
 console.log(num);
 /*Task 2 */
-const fruits = ['Orange','Banana','Grape'];
-fruits.push('Apple') 
+const fruits = ['Orange', 'Banana', 'Grape'];
+fruits.push('Apple')
 console.log(fruits)
 /*Task 3 */
-const number = [2,8,1]
+const number = [2, 8, 1]
 let total = 0
-for(let i = 0 ;i<number.length;i++){
-total += number[i]
+for (let i = 0; i < number.length; i++) {
+    total += number[i]
 }
 console.log(total)
 /*Task 4 */
-const numeral = [1,2,3,4,5]
-for(let i = 0 ; i < numeral.length; i += 1){
-console.log(numeral[i])
+const numeral = [1, 2, 3, 4, 5]
+for (let i = 0; i < numeral.length; i += 1) {
+    console.log(numeral[i])
 }
 /*Task 5 */
-const numbers = ['Pen','Marker','Pencilbox','Scissors','Eraser']
-for (let i =0 ; i< numbers.length; i++){
- if (numbers[i].length > 5){
- console.log(numbers[i])
- }
+const numbers = ['Pen', 'Marker', 'Pencilbox', 'Scissors', 'Eraser']
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i].length > 5) {
+        console.log(numbers[i])
+    }
 }
 /*Task 6 */
-const digit =[1,2,3,4,5,6,7,8,9,10];
-for(let i = 0; i< digit.length;i++){
-amount = Math.max(digit[i])
+const digit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let max = digit[0];
+for (let i = 0; i < digit.length; i++) {
+    let amount = digit[i]
+    if (amount > max) {
+        max = amount;
+    }
 
 }
-console.log(amount)
+console.log(max)
+
+
 
 /*Task 7 */
-const numeric = [1,2,3,4,5,6,7,8,9,10]
-for(let i = 0 ; i < numeric.length; i++ ){
-if (numeric[i]%2 === 0){
-console.log(numeric[i])
-}
+const numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 0; i < numeric.length; i++) {
+    if (numeric[i] % 2 === 0) {
+        console.log(numeric[i])
+    }
 }
 
 
