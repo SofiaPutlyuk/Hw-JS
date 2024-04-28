f3c44d26fc6d1413d0c55165ec2e273438e97c8a
Sofia <putlyuksophia@gmail.com>
2024-04-28 14:41:56 +0300

    hw/object

---
+++ index.js
@@ -0,0 +1,75 @@
+/*Task 1 */
+const sum = Number(prompt("Введіть суму:"))
+const bankAccount = {
+ownerName : "Marichka",
+accountNumber: 3 ,
+balance: 4000,
+deposit(sum){
+let quantity = bankAccount.balance + sum
+console.log(quantity)
+},
+withdraw(sum){
+let amount = bankAccount.balance - sum
+console.log(amount)
+},
+}
+bankAccount.deposit(sum)
+bankAccount.withdraw(sum)
+/*Task 2 */
+const degrees = prompt("Введіть температуру")
+const weather = {
+temperature: -15,
+humidity: 72,
+windSpeed: 23,
+climate(degrees){
+if(degrees < weather.temperature){
+console.log(true)
+}
+if(degrees > weather.temperature){
+console.log(false)
+}
+},
+
+}
+weather.climate(degrees)
+/*Task 3 */
+const  communication = prompt("Введіть електрону пошту :")
+const codeword = prompt("Введіть пароль: ")
+const user = {
+name:"Sophia",
+email: " ",
+password:" ",
+login(communication){
+if(communication.includes("@gmail.com")){
+this.email = communication
+}
+if(this.password.length >= 4 && this.password.length <= 16){
+this.password = codeword
+ }
+},
+}
+user.login(communication)
+console.log(user)
+/*Task 4 */
+const movie = {
+title: "Avatar",
+director: "James Cameron",
+year: 2009 ,
+rating: 8.2 ,
+grading(){
+if(movie.rating > 8){
+console.log(true)
+}
+if(movie.rating < 8){
+console.log(false)
+}
+if (movie.rating > 8){
+alert("Крутий фільм")
+}
+}
+}
+movie.grading()
+console.log(`Title : ${movie.title}`)
+console.log(`Director : ${movie.director}`)
+console.log(`Year : ${movie.year}`)
+console.log(`Rating : ${movie.rating}`)
